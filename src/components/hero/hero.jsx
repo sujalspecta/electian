082d2801d12f9c3e47327c03ae4a7472caa46b97
sujalspecta <sujalspectaclicks@outlook.com>
2026-09-25 18:11:33 +0530

    swiper package update

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,104 +1,111 @@
-import { Link } from 'react-router-dom'
-import { Navigation, A11y } from "swiper/modules";
+import { Link } from 'react-router-dom';
+import { Navigation, A11y, Parallax } from "swiper/modules"; 
 import { Swiper, SwiperSlide } from "swiper/react";
+
+// 1. Bundle Modules with Core & Feature CSS
 import 'swiper/css';
 import 'swiper/css/navigation';
-import hero1 from '../../images/slider/slide-6.jpg'
-import hero2 from '../../images/slider/slide-4.jpg'
-import hero3 from '../../images/slider/slide-8.jpg'
+import 'swiper/css/parallax'; // Added Parallax Feature Stylesheet!
+
+import hero1 from '../../images/slider/slide-6.jpg';
+import hero2 from '../../images/slider/slide-4.jpg';
+import hero3 from '../../images/slider/slide-8.jpg';
 
 const Hero = () => {
-
     const ClickHandler = () => {
-        window.scrollTo(10, 0);
-    }
+        window.scrollTo(0, 0); 
+    };
 
     return (
-
-        <section className="wpo-hero-slider-s2  wpo-hero-slider-1">
-            <div className="swiper-container">
-                <div className="swiper-wrapper">
-                    <Swiper
-                        // install Swiper modules
-                        modules={[Navigation, A11y]}
-                        spaceBetween={0}
-                        slidesPerView={1}
-                        loop={true}
-                        pagination={{ clickable: true }}
-                        speed={1800}
-                        parallax={true}
-                        navigation
-                    >
-                        <SwiperSlide>
-                            <div className="slide-inner slide-bg-image" style={{ backgroundImage: `url(${hero1})` }}>
-                                <div className="slide-inner slide-bg-image" data-background="assets/images/slider/slide-6.jpg">
-                                    <div className="container-fluid">
-                                        <div className="slide-content">
-                                            <div data-swiper-parallax="300" className="slide-title">
-                                                <h2>Life Without Liberty,
-                                                    As like Hell.</h2>
-                                            </div>
-                                            <div data-swiper-parallax="400" className="slide-text">
-                                                <p>Lorem Ipsum has been the industry's standard dummy text ever since the
-                                                    scrambled it to make.</p>
-                                            </div>
-                                            <div className="clearfix"></div>
-                                            <div data-swiper-parallax="500" className="slide-btns">
-                                                <Link onClick={ClickHandler} to="/about" className="theme-btn-s3">JOIN THE CAMPAIGN</Link>
-                                            </div>
-                                        </div>
-                                    </div>
+        <section className="wpo-hero-slider-s2 wpo-hero-slider-1" style={{ width: '100%', height: '100vh' }}>
+            <Swiper
+                modules={[Navigation, A11y, Parallax]}
+                spaceBetween={0}
+                slidesPerView={1}
+                loop={true}
+                speed={1800}
+                parallax={true}
+                navigation={true}
+                pagination={{ clickable: true }} 
+                
+                // 2. Declare explicit Dimensions upfront
+                style={{ width: "100%", height: "100%" }} 
+                
+                // 3. DOM Observers Activated
+                observer={true}
+                observeParents={true}
+                
+                // 4. Standard "Init Defibrillator" Lifecycle Hook
+                onSwiper={(swiper) => {
+                    setTimeout(() => {
+                        if (swiper && !swiper.destroyed) {
+                            swiper.update();
+                        }
+                    }, 150);
+                }}
+            >
+                {/* Slide 1 */}
+                <SwiperSlide style={{ width: "100%", height: "100%" }}>
+                    <div className="slide-inner slide-bg-image" style={{ backgroundImage: `url(${hero1})`, width: "100%", height: "100%" }}>
+                        <div className="container-fluid">
+                            <div className="slide-content">
+                                <div data-swiper-parallax="-300" className="slide-title">
+                                    <h2>Life Without Liberty, As like Hell.</h2>
+                                </div>
+                                <div data-swiper-parallax="-400" className="slide-text">
+                                    <p>Lorem Ipsum has been the industry's standard dummy text ever since.</p>
+                                </div>
+                                <div className="clearfix"></div>
+                                <div data-swiper-parallax="-500" className="slide-btns">
+                                    <Link onClick={ClickHandler} to="/about" className="theme-btn-s3">JOIN THE CAMPAIGN</Link>
                                 </div>
                             </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <div className="slide-inner slide-bg-image" style={{ backgroundImage: `url(${hero2})` }}>
-                                <div className="container-fluid">
-                                    <div className="slide-content">
-                                        <div data-swiper-parallax="300" className="slide-title">
-                                            <h2>Let's Make
-                                                America Great Again</h2>
-                                        </div>
-                                        <div data-swiper-parallax="400" className="slide-text">
-                                            <p>Lorem Ipsum has been the industry's standard dummy text ever since the
-                                                scrambled it to make.</p>
-                                        </div>
-                                        <div className="clearfix"></div>
-                                        <div data-swiper-parallax="500" className="slide-btns">
-                                            <Link onClick={ClickHandler} to="/about" className="theme-btn-s3">JOIN THE CAMPAIGN</Link>
-                                        </div>
-                                    </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+                {/* Slide 2 */}
+                <SwiperSlide style={{ width: "100%", height: "100%" }}>
+                    <div className="slide-inner slide-bg-image" style={{ backgroundImage: `url(${hero2})`, width: "100%", height: "100%" }}>
+                        <div className="container-fluid">
+                            <div className="slide-content">
+                                <div data-swiper-parallax="-300" className="slide-title">
+                                    <h2>Let's Make America Great Again</h2>
+                                </div>
+                                <div data-swiper-parallax="-400" className="slide-text">
+                                    <p>Lorem Ipsum has been the industry's standard dummy text ever since.</p>
+                                </div>
+                                <div className="clearfix"></div>
+                                <div data-swiper-parallax="-500" className="slide-btns">
+                                    <Link onClick={ClickHandler} to="/about" className="theme-btn-s3">JOIN THE CAMPAIGN</Link>
                                 </div>
                             </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <div className="slide-inner slide-bg-image" style={{ backgroundImage: `url(${hero3})` }}>
-                                <div className="container-fluid">
-                                    <div className="slide-content">
-                                        <div data-swiper-parallax="300" className="slide-title">
-                                            <h2>Life Without Liberty,
-                                                As like Hell.</h2>
-                                        </div>
-                                        <div data-swiper-parallax="400" className="slide-text">
-                                            <p>Lorem Ipsum has been the industry's standard dummy text ever since the
-                                                scrambled it to make.</p>
-                                        </div>
-                                        <div className="clearfix"></div>
-                                        <div data-swiper-parallax="500" className="slide-btns">
-                                            <Link onClick={ClickHandler} to="/about" className="theme-btn-s3">JOIN THE CAMPAIGN</Link>
-                                        </div>
-                                    </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+                {/* Slide 3 */}
+                <SwiperSlide style={{ width: "100%", height: "100%" }}>
+                    <div className="slide-inner slide-bg-image" style={{ backgroundImage: `url(${hero3})`, width: "100%", height: "100%" }}>
+                        <div className="container-fluid">
+                            <div className="slide-content">
+                                <div data-swiper-parallax="-300" className="slide-title">
+                                    <h2>Life Without Liberty, As like Hell.</h2>
+                                </div>
+                                <div data-swiper-parallax="-400" className="slide-text">
+                                    <p>Lorem Ipsum has been the industry's standard dummy text ever since.</p>
+                                </div>
+                                <div className="clearfix"></div>
+                                <div data-swiper-parallax="-500" className="slide-btns">
+                                    <Link onClick={ClickHandler} to="/about" className="theme-btn-s3">JOIN THE CAMPAIGN</Link>
                                 </div>
                             </div>
-                        </SwiperSlide>
-                        ...
-                    </Swiper>
-
-
-                </div>
-            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
         </section>
-    )
-}
+    );
+};
 
 export default Hero;
--- src/components/hero3/hero3.jsx
+++ src/components/hero3/hero3.jsx
@@ -1,82 +1,96 @@
-import { Link } from 'react-router-dom'
-import { Navigation, A11y } from "swiper/modules";
+import { Link } from 'react-router-dom';
+// 1. Added Parallax JavaScript Module
+import { Navigation, A11y, Parallax } from "swiper/modules"; 
 import { Swiper, SwiperSlide } from "swiper/react";
+
+// 2. Added Core and Feature Specific CSS
 import 'swiper/css';
 import 'swiper/css/navigation';
-import hero1 from '../../images/slider/slide-7.jpg'
-import hero2 from '../../images/slider/slide-8.jpg'
+import 'swiper/css/parallax'; 
+
+import hero1 from '../../images/slider/slide-7.jpg';
+import hero2 from '../../images/slider/slide-8.jpg';
 
 const Hero3 = () => {
 
     const ClickHandler = () => {
-        window.scrollTo(10, 0);
-    }
+        window.scrollTo(0, 0); // Corrected to absolute top (0, 0)
+    };
 
     return (
-
-        <section className="wpo-hero-slider-s2">
-            <div className="swiper-container">
-                <div className="swiper-wrapper">
-                    <Swiper
-                        // install Swiper modules
-                        modules={[Navigation, A11y]}
-                        spaceBetween={0}
-                        slidesPerView={1}
-                        loop={true}
-                        pagination={{ clickable: true }}
-                        speed={1800}
-                        parallax={true}
-                        navigation
-                    >
-                        <SwiperSlide>
-                            <div className="slide-inner slide-bg-image" style={{ backgroundImage: `url(${hero1})` }}>
-                                <div className="slide-inner slide-bg-image" data-background="assets/images/slider/slide-6.jpg">
-                                    <div className="container-fluid">
-                                        <div className="slide-content">
-                                            <div data-swiper-parallax="300" className="slide-title">
-                                                <h2>Life Without Liberty,
-                                                    As like Hell.</h2>
-                                            </div>
-                                            <div data-swiper-parallax="400" className="slide-text">
-                                                <p>Lorem Ipsum has been the industry's standard dummy text ever since the
-                                                    scrambled it to make.</p>
-                                            </div>
-                                            <div className="clearfix"></div>
-                                            <div data-swiper-parallax="500" className="slide-btns">
-                                                <Link onClick={ClickHandler} to="/about" className="theme-btn-s3">JOIN THE CAMPAIGN</Link>
-                                            </div>
-                                        </div>
-                                    </div>
+        // 3. Enforced explicit parent viewport dimensions
+        <section className="wpo-hero-slider-s2" style={{ width: '100%', height: '100vh' }}>
+            {/* 4. REMOVED manual .swiper-container and .swiper-wrapper elements */}
+            <Swiper
+                modules={[Navigation, A11y, Parallax]}
+                spaceBetween={0}
+                slidesPerView={1}
+                loop={true}
+                pagination={{ clickable: true }}
+                speed={1800}
+                parallax={true}
+                navigation={true}
+                
+                // 5. Explicitly sized the Swiper element to fill the section
+                style={{ width: "100%", height: "100%" }} 
+                
+                // 6. Configured DOM Observer guards
+                observer={true}
+                observeParents={true}
+                
+                // 7. Configured Asynchronous Init Defibrillator
+                onSwiper={(swiper) => {
+                    setTimeout(() => {
+                        if (swiper && !swiper.destroyed) {
+                            swiper.update();
+                        }
+                    }, 150);
+                }}
+            >
+                {/* Slide 1 */}
+                <SwiperSlide style={{ width: "100%", height: "100%" }}>
+                    {/* Cleaned up duplicate nested slide-inner container */}
+                    <div className="slide-inner slide-bg-image" style={{ backgroundImage: `url(${hero1})`, width: "100%", height: "100%" }}>
+                        <div className="container-fluid">
+                            <div className="slide-content">
+                                {/* Using standard negative values for smoother left-to-right parallax entry */}
+                                <div data-swiper-parallax="-300" className="slide-title">
+                                    <h2>Life Without Liberty, As like Hell.</h2>
+                                </div>
+                                <div data-swiper-parallax="-400" className="slide-text">
+                                    <p>Lorem Ipsum has been the industry's standard dummy text ever since the scrambled it to make.</p>
+                                </div>
+                                <div className="clearfix"></div>
+                                <div data-swiper-parallax="-500" className="slide-btns">
+                                    <Link onClick={ClickHandler} to="/about" className="theme-btn-s3">JOIN THE CAMPAIGN</Link>
                                 </div>
                             </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <div className="slide-inner slide-bg-image" style={{ backgroundImage: `url(${hero2})` }}>
-                                <div className="container-fluid">
-                                    <div className="slide-content">
-                                        <div data-swiper-parallax="300" className="slide-title">
-                                            <h2>World Needs A Leader</h2>
-                                        </div>
-                                        <div data-swiper-parallax="400" className="slide-text">
-                                            <p>Lorem Ipsum has been the industry's standard dummy text ever since the
-                                                scrambled it to make.</p>
-                                        </div>
-                                        <div className="clearfix"></div>
-                                        <div data-swiper-parallax="500" className="slide-btns">
-                                            <Link onClick={ClickHandler} to="/about" className="theme-btn-s3">JOIN THE CAMPAIGN</Link>
-                                        </div>
-                                    </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+                {/* Slide 2 */}
+                <SwiperSlide style={{ width: "100%", height: "100%" }}>
+                    <div className="slide-inner slide-bg-image" style={{ backgroundImage: `url(${hero2})`, width: "100%", height: "100%" }}>
+                        <div className="container-fluid">
+                            <div className="slide-content">
+                                <div data-swiper-parallax="-300" className="slide-title">
+                                    <h2>World Needs A Leader</h2>
+                                </div>
+                                <div data-swiper-parallax="-400" className="slide-text">
+                                    <p>Lorem Ipsum has been the industry's standard dummy text ever since the scrambled it to make.</p>
+                                </div>
+                                <div className="clearfix"></div>
+                                <div data-swiper-parallax="-500" className="slide-btns">
+                                    <Link onClick={ClickHandler} to="/about" className="theme-btn-s3">JOIN THE CAMPAIGN</Link>
                                 </div>
                             </div>
-                        </SwiperSlide>
-                        ...
-                    </Swiper>
-
-
-                </div>
-            </div>
+                        </div>
+                    </div>
+                </SwiperSlide>
+            </Swiper>
         </section>
-    )
-}
+    );
+};
 
 export default Hero3;
